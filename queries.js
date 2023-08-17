f6dc67471c8c252bbf20494b77ce61bf20876268
tarun <tarun.gautam@ultivic.com>
2023-08-17 12:28:54 +0530

    updates

---
+++ queries.js
@@ -0,0 +1,50 @@
+const qaData = [
+    {
+      question: "Which products do we find in apartment/home?",
+      answer: "Towels and linen, soap for dishes and hands, a piece of toilet paper, sponge for dishes and teacloth.",
+      step:1
+    },
+    {
+      question: "Troubles with safety box for keys, how it works?",
+      answer: "Look at the videos which we sent you before your arrival, insert the code and push down the black button to unlock.",
+      step:2
+    },
+    {
+        question: "Wifi; password?",
+        answer: "In apartment you’ll find a book with all informations and the wifi password. If not in the book check under the router or nera it, you’ll find it written",
+        step:3
+    },
+    {
+        question: "Is there a parking lot?",
+        answer: "Most of your homes have a parking lot, please check informations which we sent you or the book which is in your apartment. Please respect your assigned parking lot, in order to not create uneases to the other guests or owners.",
+        step:4
+    },
+    {
+        question: "Is tap water drinkable ?",
+        answer: "Yes sure. If there’ll be unforeseen communications we immediately comunicate you.",
+        step:5
+    },
+    {
+        question: "Where put rubbish?",
+        answer: "Please remember that separate garbage it’s really important to respect our beautiful environment . Outside of your home you find bins to put garbage. If you don’t remember or don’t know how to do, check on the book you have in apartment. Thanks in advance to help us in this important activity!",
+        step:6
+    },
+    {
+        question: "Missing Electricity /electricity lack",
+        answer: "Follow instruction that we sent you in the video before your arrival",
+        step:7
+    },
+    {
+        question: "It’s possibile early check in?",
+        answer: "Available on demand and if possibile at the price of € 30",
+        step:8
+    },
+    {
+        question: "It’s possibile late check out?",
+        answer: "Available on demand and if possibile at the price of € 30",
+        step:9
+    },
+    // Add more question-answer pairs as needed
+  ];
+  
+  module.exports = qaData;
